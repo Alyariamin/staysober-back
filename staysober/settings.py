b5b9aba1eec8ddde104f4a37bcb4b36da2c2b2b5
alyariamin <aminlyrr@gmail.com>
2025-07-21 01:31:49 +0330

    added allowed host

--- staysober/settings.py
+++ staysober/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-utzrv!=zm02xk#e3eogk=mnpki^6%*7^osqixqp)a*ikipdpaf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite-f1ym.onrender.com']
 
 
 # Application definition
