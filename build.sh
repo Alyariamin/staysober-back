304b3757995db881596c7405565bb7be9fc15499
alyariamin <aminlyrr@gmail.com>
2025-07-21 01:16:09 +0330

    no static files

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-if [ -z "$DISABLE_COLLESTATIC" ]; then
-  python manage.py collectstatic --noinput
-fi  
-python manage.py collectstatic --noinput
+# if [ -z "$DISABLE_COLLESTATIC" ]; then
+#   python manage.py collectstatic --noinput
+# fi  
+# python manage.py collectstatic --noinput
 
 python manage.py migrate
